1b4621320604051478ab7dc682388d342b404ef0
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2018-10-22 14:12:54 +0200

    fix images dans list pool active

--- src/app/list-pool/list-pool.component.ts
+++ src/app/list-pool/list-pool.component.ts
@@ -21,7 +21,16 @@ export class ListPoolComponent implements OnInit {
   private addImgUrl( pools ){
     for(let pool of pools){
       switch (pool['cryptocurrency']){
-        case "Bitcoin" : pool.imgUrl= "./assets/img/btc.png"
+        case "Bitcoin" : pool.imgUrl= "./assets/img/btc.png"; break;
+        case "Ethereum" : pool.imgUrl= "./assets/img/eth_logo.jpeg"; break;
+        case "XRP" : pool.imgUrl= "./assets/img/xrp.png"; break;
+        case "EthereumClassic" : pool.imgUrl= "./assets/img/etc_new.png"; break;
+        case "LiteCoin" : pool.imgUrl= "./assets/img/litecoin_logo.png"; break;
+        case "EOS" : pool.imgUrl= "./assets/img/eos_1.png"; break;
+        case "BitcoinCash" : pool.imgUrl= "./assets/img/btc_cash.png"; break;
+        case "ZCash" : pool.imgUrl= "./assets/img/zec.png"; break;
+        case "NEO" : pool.imgUrl= "./assets/img/neo.jpg"; break;
+        case "Dash" : pool.imgUrl= "./assets/img/dash.png"; break;
       }
     }
     return pools;
