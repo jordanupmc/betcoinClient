f7f8f66d1253038d42a299399e6cb40d257d9681
JustinRudat <justin.rudat@etu.upmc.fr>
2018-10-24 18:35:36 +0200

    WIP on edit user

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -10,8 +10,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor(private userservice : UserService) { }
-  urlImg;
+  constructor(private userservice: UserService) { }
   editUserForm = new FormGroup({
     passwordcur: new FormControl(''),
     passwordnew: new FormControl(''),
@@ -21,17 +20,41 @@ export class EditUserComponent implements OnInit {
     email: new FormControl(''),
     country: new FormControl('')
   });
-  
+
+  result;
+  urlImg;
+
   ngOnInit() {
-    this.urlImg= this.userservice.getIconUrl(localStorage.getItem("email"))
   }
 
-  onSubmit(){
-
+  onSubmit() {
+    const fieldName: string[] = ["password", "last_name", "first_name", "email", "country"];
+    if (this.editUserForm.value.passwordnew === this.editUserForm.value.passwordconf) {
+      const newValue: string[] = [this.editUserForm.value.passwordnew, this.editUserForm.value.lastname,
+        this.editUserForm.value.firstname, this.editUserForm.value.email, this.editUserForm.value.country];
+      let i = 0;
+      let test = 0;
+      for (i = 0; i < 5; i++) {
+        if (newValue[i] != null) {
+          test = 1;
+        }
+      }
+      if (test !== 1) {
+        this.userservice.editUser(localStorage.getItem("login"), localStorage.getItem("token"),
+          this.editUserForm.value.passwordcur, fieldName, newValue)
+          .subscribe(x => {
+              if ( x['status'] === 'KO') {
+                this.result = x['errorMessage'];
+              } else {
+                this.result = 'Successfully set up your new account informations';
+              }
+            },
+            e => console.log(e)
+          );
+      }
+    } else {
+      console.log("test")
+      this.editUserForm.value.passwordconf = 'password doesn\'t match';
+    }
   }
-
-  
-
-
-
 }
