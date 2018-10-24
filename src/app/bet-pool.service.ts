f5d98978be249060d0268677a371654883c8590f
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2018-10-24 22:44:44 +0200

    starting chat

--- src/app/bet-pool.service.ts
+++ src/app/bet-pool.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { apiURL } from './url.constants';
-
+import { Observable, of } from 'rxjs';
 import { BetPool } from './betPool';
 
 
@@ -33,5 +33,87 @@ export class BetPoolService {
     	console.log(apiURL+"quitPool?token="+token+"&login="+login+"&idPool="+idPool);
 
     return this.http.get(apiURL+"quitPool?token="+token+"&login="+login+"&idPool="+idPool);
+  }
+
+  public sendMessage(idPool, login, msg, token){
+    return this.http.post(apiURL+"messagePool", {idPool : idPool, login : login, msg : msg, token:  token});
+  }
+
+  private mock_messages = {
+    "messages": [
+      {
+          "gamblerLogin": "mick",
+          "text": "\"Salut a tous\"",
+          "messageDate": {
+              "$date": "2018-10-11T17:57:02.405Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "\"Salam les Khey\"",
+          "messageDate": {
+              "$date": "2018-10-12T16:22:56.808Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "",
+          "messageDate": {
+              "$date": "2018-10-12T16:23:38.420Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540049184335",
+          "messageDate": {
+              "$date": "2018-10-20T15:26:26.255Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540049194316",
+          "messageDate": {
+              "$date": "2018-10-20T15:26:34.416Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540049204317",
+          "messageDate": {
+              "$date": "2018-10-20T15:26:44.417Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540050668854",
+          "messageDate": {
+              "$date": "2018-10-20T15:51:10.692Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540051939278",
+          "messageDate": {
+              "$date": "2018-10-20T16:12:22.413Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540051949225",
+          "messageDate": {
+              "$date": "2018-10-20T16:12:30.361Z"
+          }
+      },
+      {
+          "gamblerLogin": "mick",
+          "text": "LOG :1540052976416",
+          "messageDate": {
+              "$date": "2018-10-20T16:29:39.462Z"
+          }
+      }
+    ]
+  };
+  public getMessage(){
+    return of (this.mock_messages); 
   }
 }
