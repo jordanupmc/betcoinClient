af1fa333441b62c86042e34cf34dd661dd12aff9
jordan.ji-ji@live.fr <jordan.ji-ji@live.fr>
2018-11-12 16:53:36 +0100

    try hack for prod issues route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
 import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
-import { LogoutComponent } from './logout/logout.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { ListPoolComponent } from './list-pool/list-pool.component';
 
 import { BetPoolDetailComponent } from './bet-pool-detail/bet-pool-detail.component';
 import { PriceChartComponent } from './price-chart/price-chart.component';
-import { ChatComponent } from './chat/chat.component';
 import { BetsComponent } from './bets/bets.component';
 import {UserAccountComponent} from './user-account/user-account.component';
 import { AuthGuardService } from './auth-guard.service';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/listPool', pathMatch: 'full' },
   { path: 'login', component:  LoginComponent},
   { path: 'subscribe', component: SubscribeComponent},
   { path: 'editUser', component: EditUserComponent,  canActivate: [AuthGuardService]},
@@ -25,7 +23,6 @@ const routes: Routes = [
   { path: 'unsubscribe', component: UnsubscribeComponent,  canActivate: [AuthGuardService]},
   { path: 'poolDetail/:id', component: BetPoolDetailComponent, canActivate: [AuthGuardService]},
   { path: 'chart', component: PriceChartComponent},
-  { path: 'chat', component: ChatComponent},
   { path: 'bets', component: BetsComponent, canActivate: [AuthGuardService]},
   { path: 'account', component: UserAccountComponent,  canActivate: [AuthGuardService]}
 ];
